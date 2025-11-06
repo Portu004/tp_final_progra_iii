bf1f99a682d979409b5465f401cc8f49140c972a
Mateo Garcia <mateoriver1@gmail.com>
2025-11-06 19:54:24 -0300

    corrijo algunas cosas del codigo

--- Tp integrador BACK/index.js
+++ Tp integrador BACK/index.js
@@ -12,33 +12,17 @@ console.log(PORT);
 
 
 
-//
-//Middlewars
-//
+
 
 app.use(cors());
 app.use(express.json());
-// Esta línea tiene que estar para que el servidor entienda los datos (JSON)
+
 
 app.get("/", (req, res) => {
     res.send("hola mundo desde express.js")
 });
 
-/*app.get("/productos", async (req, res) => {
 
-    try {
-    const sql = "SELECT * FROM productos";
-    const [rows] = await connection.query(sql);
-    console.log(rows);
-    res.status(200).json({
-        playload: rows
-    });
-
-
-    } catch (error){
-        console.error(error.message);
-    }
-});*/
 
 
 
@@ -82,53 +66,20 @@ app.get("/products/:id", async (req, res) => {
 
     try {
         // let id = req.params.id;
-        let { id } = req.params; // Aca extraemos el valor "2" de localhost:3000/products/2
-
-        // Gracias al uso de los placeholders -> ? evitamos ataques de inyeccion SQL
-        //let sql = `SELECT * FROM productos WHERE productos.id = ${id}`; // Opcion 1. Consulta no segura
-        let sql = "SELECT * FROM productos WHERE productos.id = ?"; // Opcion 2, sentencia mas segura
-
-        //let [rows] = await connection.query(sql); // Aca introducimos la consulta 1 no segura
-        let [rows] = await connection.query(sql, [id]); // Este id reemplazara el placeholder ?
+        let { id } = req.params; 
+        
+        let sql = "SELECT * FROM productos WHERE productos.id = ?"; 
+        
+        let [rows] = await connection.query(sql, [id]); 
 
         console.log(rows);
- 
+
         res.status(200).json({
             payload: rows,
             message: "Producto encontrado"
         });
 
-        /*
-        Los placeholders en SQL son marcadores especiales, como el carácter ? o nombres como :nombre, que se utilizan en consultas SQL 
-        para indicar dónde se insertarán los valores reales durante la ejecución de la consulta.
         
-        Su uso principal es prevenir inyecciones SQL al separar el código de la consulta del contenido de los datos, 
-        ya que los valores se vinculan de forma segura a los placeholders en lugar de ser incrustados directamente en la cadena de consulta.
-
-        // Gracias al destructuring, en rows guardamos y devolvemos especificamente los datos del producto, el resultado especifico de la consulta
-        //let [rows, fields] = await connection.query(sql, [id]); // Este id reemplazara el placeholder ?
-        //console.log(rows);
-        [
-            {
-                id: 2,
-                nombre: 'Led Zeppelin - Led Zeppelin II',
-                tipo: 'LP',
-                precio: 50000,
-                imagen: 'https://www.songfacts.com/img-artalbums-145-21ea7543cb15fa1143f6d2658b63f681.png',
-                activo: 1
-            }
-        ]
-
-        //console.log(fields);
-        [
-            `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-            `nombre` VARCHAR(240) NOT NULL,
-            `tipo` VARCHAR(100) NOT NULL,
-            `precio` FLOAT NOT NULL,
-            `imagen` VARCHAR(240) NOT NULL,
-            `activo` TINYINT(1) NOT NULL
-        ]*/
-
     } catch(error) {
         console.error(`Error obteniendo productos con id ${id}`, error.message);
 
@@ -141,25 +92,13 @@ app.get("/products/:id", async (req, res) => {
 })
 
 
-/* TO DO: Optimizar:
-    - GET: Seleccionar solamente los campos necesarios y devolver tambien un message junto al payload
-    - GET by id: Meter optimizaciones
-*/
 
 
-// POST -> Crear un nuevo producto
+
 app.post("/products", async (req, res) => {
 
     try {
-        /*  image: "johnnymelavo.com"
-            name: "Johnny Melavo"
-            price: "12"
-            type: "CD"
-        */
-
-        // TODO, recordar el middleware express.json()
-
-        // Gracias al destructuring, recogemos estos datos del body
+        
         let { image, name, price, type } = req.body;
 
         console.log(req.body);
@@ -169,7 +108,7 @@ app.post("/products", async (req, res) => {
         let [rows] = await connection.query(sql, [image, name, price, type]);
 
         // Codigo de estado 201 -> Created
-        res.status(201).json({
+        res.status(200).json({
             message: "Producto creado con exito"
         });
 
@@ -189,11 +128,10 @@ app.delete("/products/:id", async (req, res) => {
     try {
         let { id } = req.params;
 
-        // Opcion 1: Borrado normal
+        
         let sql = "DELETE FROM productos WHERE id = ?";
 
-        // Opcion 2: Baja logica
-        //let sql = "UPDATE productos set active = 0 WHERE id = ?";
+        
 
         let [result] = await connection.query(sql, [id]);
         console.log(result);

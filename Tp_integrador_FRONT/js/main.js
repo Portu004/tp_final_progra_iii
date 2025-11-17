66fe8ec1ab19222c06a721de7d0937ecd3dbaac9
Mateo Garcia <mateoriver1@gmail.com>
2025-11-17 19:48:44 -0300

    corrigiendo cosas del html

--- Tp_integrador_FRONT/js/main.js
+++ Tp_integrador_FRONT/js/main.js
@@ -2,7 +2,7 @@ let contenedorProductos = document.getElementById("contenedor-productos");
 
 async function obtenerProductos() {
     try {
-        let respuesta = await fetch("http://localhost:3000/products"); 
+        let respuesta = await fetch("http://localhost:3000/api/products"); 
         
         let respuestaFormato = await respuesta.json(); // Esto es { playload: [...] }
         
